c44923af16e6b25d79840e45355fc254d0b8ecca
Илья <gosha.shlapak6982@gmail.com>
2022-09-14 22:35:47 +0300

    Second Commit hw10

--- src/Book.java
+++ src/Book.java
@@ -1,23 +1,49 @@
+import java.util.Objects;
 public class Book {
     private final String name;
     private final Author Author;
     private Integer year;
-    public Book(String name, Author Author, int year){
 
-        this.name=name;
+    public Book(String name, Author Author, int year) {
+
+        this.name = name;
         this.Author = Author;
-        this.year=year;
+        this.year = year;
     }
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
-    public Author getAuthor(){
+
+    public Author getAuthor() {
         return this.Author;
     }
-    public int getYear(){
+
+    public int getYear() {
         return this.year;
     }
-    public void setYear(int year){
-        this.year=year;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Name =" + ' ' + this.name + ", Author = " + this.Author + ", Year = " + this.year + ' ';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return year == book.year && name.equals(book.name) && Author.equals(book.Author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, Author, year);
     }
 }
